dacde68ff12dcb5da8992d7dd69d04f3b72eaa64
karol509x <karol509x@gmail.com>
2017-08-27 13:31:12 +0200

    scrolowanie

--- js/INFINITE SCROLL.js
+++ js/INFINITE SCROLL.js
@@ -1,4 +1,20 @@
 'use strict';
 
 
-alert('loool');
+$(function () {
+    $(window).scroll(function () {
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            $.ajax({
+                url: 'https://jsonplaceholder.typicode.com/users',
+                dataType: 'json',
+                success: function(response) {
+                    
+                },
+                onerror: function(error) {
+                    
+                    
+                }
+            });
+        }
+    });
+});
