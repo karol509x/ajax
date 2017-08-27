22dc5f78ce1e4c199b6c1101e4d2c3fb408490f0
karol509x <karol509x@gmail.com>
2017-08-27 11:31:36 +0200

    bottom-warsztat

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,31 @@
 'use strict';
+
+$(function () {
+    $.ajax({
+        url: "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+        dataType: 'json',
+        success: function (resultJSON) {
+            console.log(resultJSON);
+            
+            var pUserId = document.createElement('p');
+            var pUserName = document.createElement('p');
+            var pUserUrl = document.createElement('p');
+            
+            pUserId.innerHTML = 'User ID: ' + resultJSON.userId;
+            pUserName.innerHTML = 'User ID: ' + resultJSON.userName;
+            pUserUrl.innerHTML = 'User url http://' + resultJSON.userURL;
+            
+            document.body.appendChild(pUserId);   
+            document.body.appendChild(pUserName);
+            document.body.appendChild(pUserUrl);
+        },
+        onerror: function (msg) {
+            console.log(msg);
+        }
+    });
+    
+   /* $getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', 
+        function(data) {
+        console.log(data);
+    });*/
+});
